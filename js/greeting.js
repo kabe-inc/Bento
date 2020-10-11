10fbf7f70910dde41a82458981a7b82b8b30dc45
kabe-inc <72587902+kabe-inc@users.noreply.github.com>
2020-10-11 16:38:01 +0700

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,14 +3,14 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change your name
-var name = 'Hugh';
+var name = 'Hugh!';
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
-var gree3 = 'Good afternoon ';
-var gree4 = 'Good evening ';
-var gree5 = 'Good evening ';
+var gree1 = 'Go to sleep, ';
+var gree2 = 'Good morning, ';
+var gree3 = 'Good afternoon, ';
+var gree4 = 'Good evening, ';
+var gree5 = 'Good evening, ';
 var gree6 = 'Good evening ';
 
 // Define the hours of the greetings
